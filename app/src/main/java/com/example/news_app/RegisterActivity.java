c7bea9684396b7855f49538310f6abe449b48475
alexYamaoka <ryamaoka1@gmail.com>
2020-04-06 02:17:02 -0700

    added login and register activity

---
+++ app/src/main/java/com/example/news_app/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.example.news_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
