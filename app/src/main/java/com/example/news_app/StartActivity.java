eac9dc92c9bc11fd352b40626ec6b5ffdc96a7ff
alexYamaoka <ryamaoka1@gmail.com>
2020-04-06 02:19:48 -0700

    added start activity

---
+++ app/src/main/java/com/example/news_app/StartActivity.java
@@ -0,0 +1,16 @@
+package com.example.news_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StartActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
