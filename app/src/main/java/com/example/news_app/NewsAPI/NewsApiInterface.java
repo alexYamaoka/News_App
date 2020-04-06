0f616756034a78ebf667f29f0b32e443008a998d
alexYamaoka <ryamaoka1@gmail.com>
2020-04-06 02:15:15 -0700

    added news api interface

---
+++ app/src/main/java/com/example/news_app/NewsAPI/NewsApiInterface.java
@@ -0,0 +1,16 @@
+package com.example.news_app.NewsAPI;
+
+import com.example.news_app.Models.News;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface NewsApiInterface
+{
+    @GET("top-headlines")
+    Call<News> getTopHeadlinesNews(@Query("country") String country, @Query("apiKey") String apiKey);
+
+    @GET("everything")
+    Call<News> getNewsSearch(@Query("q") String keyword, @Query("apiKey") String apiKey);
+}
